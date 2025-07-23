f24707836385c0ab72c03a36a61f7937ab4c6c71
Siddhesh-78 <siddheshbangar0@gmail.com>
2025-07-24 00:26:42 +0530

    fix: set correct base path for Vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Sid/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: './', // ⬅️ VERY IMPORTANT for GitHub + Vercel
 })
+
